2b4515ad57a4557db724e596eaf5adc05777913c
Casey Brown <115745142+caseynbrown@users.noreply.github.com>
2022-12-05 18:29:21 -0500

    Add files via upload

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -30,3 +30,62 @@ GROUP BY title
 ORDER BY COUNT(title) DESC;
 
 SELECT * FROM retirement_titles
+
+--Create retirement_titles table
+SELECT E.emp_no
+	,E.first_name
+	,E.last_name
+	,T.title
+	,T.from_date
+	,T.to_date
+INTO retirement_titles
+FROM employees E
+JOIN titles T
+ON E.emp_no= T.emp_no
+WHERE (E.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY E.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, title DESC;
+
+--Retrieve the number of employees by their most recent job title who are about to retire. 
+SELECT COUNT(ut.emp_no),
+ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT * FROM retirement_titles
+
+SELECT * FROM unique_titles
+
+SELECT * FROM retiring_titles
+ORDER BY COUNT(title) DESC;
+
+SELECT * FROM retiring_titles
+
+-- Write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibilty
